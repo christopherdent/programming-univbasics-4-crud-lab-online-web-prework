62b02859a1040517ba04c2dfb9fa083604775ad3
Christopher Dent <christopherdent01@gmail.com>
2019-06-21 00:54:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,8 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["I", "am", "really", "learning", "arrays!"]
-  array.pop = arrays!
+  arrays! = array.pop
+  p 
 end
 
 def remove_element_from_start_of_array(array)
