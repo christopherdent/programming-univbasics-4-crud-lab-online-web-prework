293a2c89ba2c003464ebc2731df6bca8c54dfd1d
Christopher Dent <christopherdent01@gmail.com>
2019-06-21 00:36:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
   
 end
 
-def add_element_to_end_of_array("avengers", "Captain America")
-  avengers << "Captain America"
+def add_element_to_end_of_array(avengers, Captain)
+  avengers << "Captain"
 end
 
 def add_element_to_start_of_array(array, element)
