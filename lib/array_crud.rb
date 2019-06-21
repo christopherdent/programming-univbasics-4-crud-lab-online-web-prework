ee6f89ed52ee26bc078ade37a2364225016b09e5
Christopher Dent <christopherdent01@gmail.com>
2019-06-21 00:53:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  avengers = ["Hulk", "Iron Man", "Thor", "Ant-Man"]
-  avengers.push("arrays!")
+  array = ["wow", "I", "am", "really", "learning"]
+  array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
